598208cdb5b91bf298d482dd8373d8287a41ed0f
Jeremy Frick <jeremy.frick@me.com>
2023-11-14 19:59:16 -0800

    Initial Commit

---
+++ SimpleTooDoo/SimpleTooDooApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTooDooApp.swift
+//  SimpleTooDoo
+//
+//  Created by Jeremy Frick on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTooDooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
